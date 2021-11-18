80f59d0e38fbed8ce795512036cae894cfb26a9b
Abraham Lomely <abraham-ba-18@hotmail.com>
2021-11-18 09:42:06 -0700

    updated my classes

--- src/main/java/Models/Branch.java
+++ src/main/java/Models/Branch.java
@@ -7,258 +7,249 @@ import java.util.*;
 // line 14 "model.ump"
 // line 118 "model.ump"
 // line 177 "model.ump"
-public class Branch
-{
+public class Branch {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+    //------------------------
+    // MEMBER VARIABLES
+    //------------------------
 
-  //Branch Attributes
-  private String address;
+    //Branch Attributes
+    private String address;
 
-  //Branch Associations
-  private BranchMenu branchMenu;
-  private Chain chain;
-  private final List<Employee> employees;
-  private Stock stock;
+    //Branch Associations
+    private BranchMenu branchMenu;
+    private Chain chain;
+    private final List<Employee> employees;
+    private Stock stock;
+    private int minimumNumberOfEmployees;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+    //------------------------
+    // CONSTRUCTOR
+    //------------------------
 
-  public Branch(String aAddress, BranchMenu aBranchMenu, Chain aChain, Stock aStock)
-  {
-    address = aAddress;
-    if (aBranchMenu == null || aBranchMenu.getBranch() != null)
-    {
-      throw new RuntimeException("Unable to create Branch due to aBranchMenu. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    branchMenu = aBranchMenu;
-    boolean didAddChain = setChain(aChain);
-    if (!didAddChain)
-    {
-      throw new RuntimeException("Unable to create branch due to chain. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    employees = new ArrayList<Employee>();
-    if (aStock == null || aStock.getBranch() != null)
-    {
-      throw new RuntimeException("Unable to create Branch due to aStock. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    stock = aStock;
-  }
-
-  public Branch(String aAddress, CorpMenu aCorpMenuForBranchMenu, Chain aChain)
-  {
-    address = aAddress;
-    branchMenu = new BranchMenu(aCorpMenuForBranchMenu, this);
-    boolean didAddChain = setChain(aChain);
-    if (!didAddChain)
-    {
-      throw new RuntimeException("Unable to create branch due to chain. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-    }
-    employees = new ArrayList<Employee>();
-    stock = new Stock(this);
-  }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-
-  public boolean setAddress(String aAddress)
-  {
-    boolean wasSet = false;
-    address = aAddress;
-    wasSet = true;
-    return wasSet;
-  }
-
-  public String getAddress()
-  {
-    return address;
-  }
-  /* Code from template association_GetOne */
-  public BranchMenu getBranchMenu()
-  {
-    return branchMenu;
-  }
-  /* Code from template association_GetOne */
-  public Chain getChain()
-  {
-    return chain;
-  }
-  /* Code from template association_GetMany */
-  public Employee getEmployee(int index)
-  {
-    Employee aEmployee = employees.get(index);
-    return aEmployee;
-  }
-
-  public List<Employee> getEmployees()
-  {
-    List<Employee> newEmployees = Collections.unmodifiableList(employees);
-    return newEmployees;
-  }
-
-  public int numberOfEmployees()
-  {
-    int number = employees.size();
-    return number;
-  }
-
-  public boolean hasEmployees()
-  {
-    boolean has = employees.size() > 0;
-    return has;
-  }
-
-  public int indexOfEmployee(Employee aEmployee)
-  {
-    int index = employees.indexOf(aEmployee);
-    return index;
-  }
-  /* Code from template association_GetOne */
-  public Stock getStock()
-  {
-    return stock;
-  }
-  /* Code from template association_SetOneToMany */
-  public boolean setChain(Chain aChain)
-  {
-    boolean wasSet = false;
-    if (aChain == null)
-    {
-      return wasSet;
+    public Branch(String aAddress, BranchMenu aBranchMenu, Chain aChain, Stock aStock) {
+        address = aAddress;
+        if (aBranchMenu == null || aBranchMenu.getBranch() != null) {
+            throw new RuntimeException("Unable to create Branch due to aBranchMenu. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+        branchMenu = aBranchMenu;
+        boolean didAddChain = setChain(aChain);
+        if (!didAddChain) {
+            throw new RuntimeException("Unable to create branch due to chain. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+        employees = new ArrayList<Employee>();
+        if (aStock == null || aStock.getBranch() != null) {
+            throw new RuntimeException("Unable to create Branch due to aStock. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+        stock = aStock;
+        this.minimumNumberOfEmployees = minimumNumberOfEmployees;
     }
 
-    Chain existingChain = chain;
-    chain = aChain;
-    if (existingChain != null && !existingChain.equals(aChain))
-    {
-      existingChain.removeBranch(this);
+    public Branch(String aAddress, CorpMenu aCorpMenuForBranchMenu, Chain aChain) {
+        address = aAddress;
+        branchMenu = new BranchMenu(aCorpMenuForBranchMenu, this);
+        boolean didAddChain = setChain(aChain);
+        if (!didAddChain) {
+            throw new RuntimeException("Unable to create branch due to chain. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+        employees = new ArrayList<Employee>();
+        stock = new Stock(this);
     }
-    chain.addBranch(this);
-    wasSet = true;
-    return wasSet;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfEmployees()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Employee addEmployee(String aName, int aId, int aSalary, float aTimeWorked, Order... allOrders)
-        //(String aName, int aId, int aSalary, float aTimeWorked, enum aType, enum aRole, Order... allOrders)
-  {
-    return new Employee(aName, aId, aSalary, aTimeWorked, this, allOrders);
-  }
 
-  public boolean addEmployee(Employee aEmployee)
-  {
-    boolean wasAdded = false;
-    if (employees.contains(aEmployee)) { return false; }
-    Branch existingBranch = aEmployee.getBranch();
-    boolean isNewBranch = existingBranch != null && !this.equals(existingBranch);
-    if (isNewBranch)
-    {
-      aEmployee.setBranch(this);
-    }
-    else
-    {
-      employees.add(aEmployee);
-    }
-    wasAdded = true;
-    return wasAdded;
-  }
+    //------------------------
+    // INTERFACE
+    //------------------------
 
-  public boolean removeEmployee(Employee aEmployee)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aEmployee, as it must always have a branch
-    if (!this.equals(aEmployee.getBranch()))
-    {
-      employees.remove(aEmployee);
-      wasRemoved = true;
+    public boolean setAddress(String aAddress) {
+        boolean wasSet = false;
+        address = aAddress;
+        wasSet = true;
+        return wasSet;
     }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addEmployeeAt(Employee aEmployee, int index)
-  {  
-    boolean wasAdded = false;
-    if(addEmployee(aEmployee))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfEmployees()) { index = numberOfEmployees() - 1; }
-      employees.remove(aEmployee);
-      employees.add(index, aEmployee);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
 
-  public boolean addOrMoveEmployeeAt(Employee aEmployee, int index)
-  {
-    boolean wasAdded = false;
-    if(employees.contains(aEmployee))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfEmployees()) { index = numberOfEmployees() - 1; }
-      employees.remove(aEmployee);
-      employees.add(index, aEmployee);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addEmployeeAt(aEmployee, index);
+    public String getAddress() {
+        return address;
     }
-    return wasAdded;
-  }
 
-  public void delete()
-  {
-    BranchMenu existingBranchMenu = branchMenu;
-    branchMenu = null;
-    if (existingBranchMenu != null)
-    {
-      existingBranchMenu.delete();
+    /* Code from template association_GetOne */
+    public BranchMenu getBranchMenu() {
+        return branchMenu;
     }
-    Chain placeholderChain = chain;
-    this.chain = null;
-    if(placeholderChain != null)
-    {
-      placeholderChain.removeBranch(this);
+
+    /* Code from template association_GetOne */
+    public Chain getChain() {
+        return chain;
     }
-    for(int i=employees.size(); i > 0; i--)
-    {
-      Employee aEmployee = employees.get(i - 1);
-      aEmployee.delete();
+
+    /* Code from template association_GetMany */
+    public Employee getEmployee(int index) {
+        Employee aEmployee = employees.get(index);
+        return aEmployee;
     }
-    Stock existingStock = stock;
-    stock = null;
-    if (existingStock != null)
-    {
-      existingStock.delete();
+
+    public List<Employee> getEmployees() {
+        List<Employee> newEmployees = Collections.unmodifiableList(employees);
+        return newEmployees;
     }
-  }
 
-  // line 17 "model.ump"
-   public void add(String employee){
-    
-  }
+    public int numberOfEmployees() {
+        int number = employees.size();
+        return number;
+    }
 
-  // line 19 "model.ump"
-  public boolean remove(String employee){
-    return true;
-  }
+    public boolean hasEmployees() {
+        boolean has = employees.size() > 0;
+        return has;
+    }
+
+    public int indexOfEmployee(Employee aEmployee) {
+        int index = employees.indexOf(aEmployee);
+        return index;
+    }
+
+    /* Code from template association_GetOne */
+    public Stock getStock() {
+        return stock;
+    }
+
+    /* Code from template association_SetOneToMany */
+    public boolean setChain(Chain aChain) {
+        boolean wasSet = false;
+        if (aChain == null) {
+            return wasSet;
+        }
+
+        Chain existingChain = chain;
+        chain = aChain;
+        if (existingChain != null && !existingChain.equals(aChain)) {
+            existingChain.removeBranch(this);
+        }
+        chain.addBranch(this);
+        wasSet = true;
+        return wasSet;
+    }
+
+    /* Code from template association_MinimumNumberOfMethod */
+    public int geMinimumNumberOfEmployees() {
+        return this.minimumNumberOfEmployees;
+    }
+
+    /* Code from template association_AddManyToOne */
+    public Employee addEmployee(String aName, int aId, int aSalary, Branch aBranch, Order... allOrders)
+    //(String aName, int aId, int aSalary, float aTimeWorked, enum aType, enum aRole, Order... allOrders)
+    {
+        return new Employee(aName, aId, aSalary, aBranch, allOrders);
+    }
+
+    public boolean addEmployee(Employee aEmployee) {
+        boolean wasAdded = false;
+        if (employees.contains(aEmployee)) {
+            return false;
+        }
+        Branch existingBranch = aEmployee.getBranch();
+        boolean isNewBranch = existingBranch != null && !this.equals(existingBranch);
+        if (isNewBranch) {
+            aEmployee.setBranch(this);
+        } else {
+            employees.add(aEmployee);
+        }
+        wasAdded = true;
+        return wasAdded;
+    }
+
+    public boolean removeEmployee(Employee aEmployee) {
+        boolean wasRemoved = false;
+        //Unable to remove aEmployee, as it must always have a branch
+        if (!this.equals(aEmployee.getBranch())) {
+            employees.remove(aEmployee);
+            wasRemoved = true;
+        }
+        return wasRemoved;
+    }
+
+    /* Code from template association_AddIndexControlFunctions */
+    public boolean addEmployeeAt(Employee aEmployee, int index) {
+        boolean wasAdded = false;
+        if (addEmployee(aEmployee)) {
+            if (index < 0) {
+                index = 0;
+            }
+            if (index > numberOfEmployees()) {
+                index = numberOfEmployees() - 1;
+            }
+            employees.remove(aEmployee);
+            employees.add(index, aEmployee);
+            wasAdded = true;
+        }
+        return wasAdded;
+    }
+
+    public boolean addOrMoveEmployeeAt(Employee aEmployee, int index) {
+        boolean wasAdded = false;
+        if (employees.contains(aEmployee)) {
+            if (index < 0) {
+                index = 0;
+            }
+            if (index > numberOfEmployees()) {
+                index = numberOfEmployees() - 1;
+            }
+            employees.remove(aEmployee);
+            employees.add(index, aEmployee);
+            wasAdded = true;
+        } else {
+            wasAdded = addEmployeeAt(aEmployee, index);
+        }
+        return wasAdded;
+    }
+
+    public void delete() {
+        BranchMenu existingBranchMenu = branchMenu;
+        branchMenu = null;
+        if (existingBranchMenu != null) {
+            existingBranchMenu.delete();
+        }
+        Chain placeholderChain = chain;
+        this.chain = null;
+        if (placeholderChain != null) {
+            placeholderChain.removeBranch(this);
+        }
+        for (int i = employees.size(); i > 0; i--) {
+            Employee aEmployee = employees.get(i - 1);
+            aEmployee.delete();
+        }
+        Stock existingStock = stock;
+        stock = null;
+        if (existingStock != null) {
+            existingStock.delete();
+        }
+    }
+
+    // line 17 "model.ump"
+    public boolean add(Employee employee) {
+        try {
+            this.employees.add(employee);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // line 19 "model.ump"
+    public boolean remove(String employee) {
+        try {
+            this.employees.remove(employee);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 
-  public String toString()
-  {
-    return super.toString() + "["+
-            "address" + ":" + getAddress()+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "branchMenu = "+(getBranchMenu()!=null?Integer.toHexString(System.identityHashCode(getBranchMenu())):"null") + System.getProperties().getProperty("line.separator") +
-            "  " + "chain = "+(getChain()!=null?Integer.toHexString(System.identityHashCode(getChain())):"null") + System.getProperties().getProperty("line.separator") +
-            "  " + "stock = "+(getStock()!=null?Integer.toHexString(System.identityHashCode(getStock())):"null");
-  }
+    public String toString() {
+        return super.toString() + "[" +
+                "address" + ":" + getAddress() + "]" + System.getProperties().getProperty("line.separator") +
+                "  " + "branchMenu = " + (getBranchMenu() != null ? Integer.toHexString(System.identityHashCode(getBranchMenu())) : "null") + System.getProperties().getProperty("line.separator") +
+                "  " + "chain = " + (getChain() != null ? Integer.toHexString(System.identityHashCode(getChain())) : "null") + System.getProperties().getProperty("line.separator") +
+                "  " + "stock = " + (getStock() != null ? Integer.toHexString(System.identityHashCode(getStock())) : "null");
+    }
 }
--- src/main/java/Models/Chain.java
+++ src/main/java/Models/Chain.java
@@ -7,181 +7,174 @@ import java.util.*;
 // line 38 "model.ump"
 // line 162 "model.ump"
 // line 187 "model.ump"
-public class Chain
-{
+public class Chain {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+    //------------------------
+    // MEMBER VARIABLES
+    //------------------------
 
-  //Chain Associations
-  private final List<Branch> branchs;
-  private CorpMenu corpMenu;
+    //Chain Associations
+    private List<Branch> branches;
+    private CorpMenu corpMenu;
+    private int minimumNumber;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+    //------------------------
+    // CONSTRUCTOR
+    //------------------------
 
-  public Chain(CorpMenu aCorpMenu)
-  {
-    branchs = new ArrayList<Branch>();
-    if (aCorpMenu == null || aCorpMenu.getChain() != null)
-    {
-      throw new RuntimeException("Unable to create Chain due to aCorpMenu. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+    public Chain(CorpMenu aCorpMenu, int minimumNumber) {
+        branches = new ArrayList<Branch>();
+        this.minimumNumber = minimumNumber;
+        if (aCorpMenu == null || aCorpMenu.getChain() != null) {
+            throw new RuntimeException("Unable to create Chain due to aCorpMenu. See http://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+        corpMenu = aCorpMenu;
     }
-    corpMenu = aCorpMenu;
-  }
 
-  public Chain()
-  {
-    branchs = new ArrayList<Branch>();
-    corpMenu = new CorpMenu(this);
-  }
-
-  //------------------------
-  // INTERFACE
-  //------------------------
-  /* Code from template association_GetMany */
-  public Branch getBranch(int index)
-  {
-    Branch aBranch = branchs.get(index);
-    return aBranch;
-  }
-
-  public List<Branch> getBranchs()
-  {
-    List<Branch> branches = Collections.unmodifiableList(branchs);
-    return branches;
-  }
-
-  public int numberOfBranchs()
-  {
-    int number = branchs.size();
-    return number;
-  }
-
-  public boolean hasBranchs()
-  {
-    boolean has = branchs.size() > 0;
-    return has;
-  }
-
-  public int indexOfBranch(Branch aBranch)
-  {
-    int index = branchs.indexOf(aBranch);
-    return index;
-  }
-  /* Code from template association_GetOne */
-  public CorpMenu getCorpMenu()
-  {
-    return corpMenu;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfBranchs()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToOne */
-  public Branch addBranch(String aAddress, BranchMenu aBranchMenu, Stock aStock)
-  {
-    return new Branch(aAddress, aBranchMenu, this, aStock);
-  }
-
-  public boolean addBranch(Branch aBranch)
-  {
-    boolean wasAdded = false;
-    if (branchs.contains(aBranch)) { return false; }
-    Chain existingChain = aBranch.getChain();
-    boolean isNewChain = existingChain != null && !this.equals(existingChain);
-    if (isNewChain)
-    {
-      aBranch.setChain(this);
+    public Chain() {
+        branches = new ArrayList<Branch>();
+        corpMenu = new CorpMenu(this.corpMenu);
     }
-    else
-    {
-      branchs.add(aBranch);
+
+    public Chain(CorpMenu corpMenu) {}
+
+    //------------------------
+    // INTERFACE
+    //------------------------
+    /* Code from template association_GetMany */
+    public Branch getBranch(int index) {
+        Branch aBranch = branches.get(index);
+        return aBranch;
     }
-    wasAdded = true;
-    return wasAdded;
-  }
 
-  public boolean removeBranch(Branch aBranch)
-  {
-    boolean wasRemoved = false;
-    //Unable to remove aBranch, as it must always have a chain
-    if (!this.equals(aBranch.getChain()))
-    {
-      branchs.remove(aBranch);
-      wasRemoved = true;
+    public List<Branch> getBranches() {
+        List<Branch> branches = Collections.unmodifiableList(this.branches);
+        return branches;
     }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addBranchAt(Branch aBranch, int index)
-  {  
-    boolean wasAdded = false;
-    if(addBranch(aBranch))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfBranchs()) { index = numberOfBranchs() - 1; }
-      branchs.remove(aBranch);
-      branchs.add(index, aBranch);
-      wasAdded = true;
+
+    public int numberOfBranches() {
+        int number = branches.size();
+        return number;
     }
-    return wasAdded;
-  }
 
-  public boolean addOrMoveBranchAt(Branch aBranch, int index)
-  {
-    boolean wasAdded = false;
-    if(branchs.contains(aBranch))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfBranchs()) { index = numberOfBranchs() - 1; }
-      branchs.remove(aBranch);
-      branchs.add(index, aBranch);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addBranchAt(aBranch, index);
+    public boolean hasBranches() {
+        boolean has = branches.size() > 0;
+        return has;
     }
-    return wasAdded;
-  }
 
-  public void delete()
-  {
-    for(int i=branchs.size(); i > 0; i--)
-    {
-      Branch aBranch = branchs.get(i - 1);
-      aBranch.delete();
+    public int indexOfBranch(Branch aBranch) {
+        int index = branches.indexOf(aBranch);
+        return index;
     }
-    CorpMenu existingCorpMenu = corpMenu;
-    corpMenu = null;
-    if (existingCorpMenu != null)
-    {
-      existingCorpMenu.delete();
+
+    /* Code from template association_GetOne */
+    public CorpMenu getCorpMenu() {
+        return corpMenu;
     }
-  }
 
-  // line 42 "model.ump"
-  public List<Employee> getManagers(){
-    return new ArrayList<Employee>();
-  }
+    /* Code from template association_MinimumNumberOfMethod */
+    public int minimumNumberOfBranches() {
+        return this.minimumNumber;
+    }
 
-  // line 44 "model.ump"
-  public int numBranch(){
-    return 0;
-  }
+    /* Code from template association_AddManyToOne */
+    public Branch addBranch(String aAddress, BranchMenu aBranchMenu, Stock aStock) {
+        var newBranch = new Branch(aAddress, aBranchMenu, this, aStock);
+        this.branches.add(newBranch);
+        return newBranch;
+    }
 
-  // line 46 "model.ump"
-   public void addBranch(String Branch){
-    
-  }
+    public boolean addBranch(Branch aBranch) {
+        boolean wasAdded = false;
+        if (branches.contains(aBranch)) {
+            return false;
+        }
+        Chain existingChain = aBranch.getChain();
+        boolean isNewChain = existingChain != null && !this.equals(existingChain);
+        if (isNewChain) {
+            aBranch.setChain(this);
+        } else {
+            branches.add(aBranch);
+        }
+        wasAdded = true;
+        return wasAdded;
+    }
 
-  // line 48 "model.ump"
-   public boolean reamoveBranch(String Branch){
-    return true;
-  }
+    public boolean removeBranch(Branch aBranch) {
+        boolean wasRemoved = false;
+        //Unable to remove aBranch, as it must always have a chain
+        if (!this.equals(aBranch.getChain())) {
+            branches.remove(aBranch);
+            wasRemoved = true;
+        }
+        return wasRemoved;
+    }
+
+    /* Code from template association_AddIndexControlFunctions */
+    public boolean addBranchAt(Branch aBranch, int index) {
+        boolean wasAdded = false;
+        if (addBranch(aBranch)) {
+            if (index < 0) {
+                index = 0;
+            }
+            if (index > numberOfBranches()) {
+                index = numberOfBranches() - 1;
+            }
+            branches.remove(aBranch);
+            branches.add(index, aBranch);
+            wasAdded = true;
+        }
+        return wasAdded;
+    }
+
+    public boolean addOrMoveBranchAt(Branch aBranch, int index) {
+        boolean wasAdded = false;
+        if (branches.contains(aBranch)) {
+            if (index < 0) {
+                index = 0;
+            }
+            if (index > numberOfBranches()) {
+                index = numberOfBranches() - 1;
+            }
+            branches.remove(aBranch);
+            branches.add(index, aBranch);
+            wasAdded = true;
+        } else {
+            wasAdded = addBranchAt(aBranch, index);
+        }
+        return wasAdded;
+    }
+
+    public void delete() {
+        for (int i = branches.size(); i > 0; i--) {
+            Branch aBranch = branches.get(i - 1);
+            aBranch.delete();
+        }
+        CorpMenu existingCorpMenu = corpMenu;
+        corpMenu = null;
+        if (existingCorpMenu != null) {
+            existingCorpMenu.delete();
+        }
+    }
+
+    // line 42 "model.ump"
+    public List<Employee> getManagers() {
+        return new ArrayList<Employee>();
+    }
+
+    // line 44 "model.ump"
+    public int numBranch() {
+        return 0;
+    }
+
+    // line 46 "model.ump"
+    public void addBranch(String Branch) {
+
+    }
+
+    // line 48 "model.ump"
+    public boolean removeBranch(String Branch) {
+        return true;
+    }
 
 }
